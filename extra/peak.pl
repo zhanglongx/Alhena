8c367d2f3d49cf1a37a964611b1d564d668ffcb3
zhlx <zhanglongx@gmail.com>
2015-02-10 13:35:59 +0800

    add filename output to peak.pl

--- extra/peak.pl
+++ extra/peak.pl
@@ -2,8 +2,14 @@
 
 while(<>)
 {
+    my $filename = $ARGV;
+    
+    $filename =~ m/([a-zA-Z]+[0-9]+)/;
+    $filename = $1;
+    
     if( /^stat.*/ )
     {
+        print "$filename,";
         print;
     }
 }
