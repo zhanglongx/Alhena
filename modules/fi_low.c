2dbfd707679c9cfaffd4351f8f8a05ebf2e68c93
zhlx <zhanglongx@gmail.com>
2015-03-08 16:38:59 +0800

    modify fi_low.c, remove duplicate fi look back

--- modules/fi_low.c
+++ modules/fi_low.c
@@ -58,7 +58,6 @@ bool alhena_module_filow_pos( void *h, alhena_data_t *p_data,
     fi_low_t *p_fi = (fi_low_t *)h;
     int i_length = p_fi->i_compare_days;
     int i, i_start = i_day - i_length - p_fi->i_look_back;
-    bool b_fi_high = false;
 
     if( i_start < 0 )
         return false;
@@ -66,23 +65,14 @@ bool alhena_module_filow_pos( void *h, alhena_data_t *p_data,
 #define RATIO1   (4.0f)
 #define RATIO2   (4.0f)
 
+    if( p_fi->fi[i_day] > avg_v( p_fi->fi, i_day, i_length )
+                          - RATIO2 * dev_v( p_fi->fi, i_day, i_length ) )
+        return false;
+
     for( i=i_day - p_fi->i_look_back; i<i_day; i++ )
     {
         if( p_fi->fi[i] > avg_v( p_fi->fi, i, i_length ) 
                           + RATIO1 * dev_v( p_fi->fi, i, i_length ) )
-        {
-            b_fi_high = true;
-            break;
-        }
-    }
-
-    if( b_fi_high == false )
-        return false;
-
-    for( ; i<i_day; i++ )
-    {
-        if( p_fi->fi[i] < avg_v( p_fi->fi, i, i_length )
-                          - RATIO2 * dev_v( p_fi->fi, i, i_length ) )
             return true;
     }
 
