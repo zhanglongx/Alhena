172e2c2259a7993fd2f9528bb715e19938dc92e3
zhlx <zhanglongx@gmail.com>
2015-03-10 15:13:56 +0800

    change peak_low.c to up_days, go_down, lowest, highest

--- modules/peak_low.c
+++ modules/peak_low.c
@@ -2,11 +2,17 @@
 #include "analyser/modules.h"
 
 #include "peak_low.h"
+#include "fi.h"
 
 typedef struct _peak_low_t
 {
     int     i_max_stat_days;
     int     i_records;
+
+    float   fi[MAX_DAYS];
+
+    int     up_days[MAX_DAYS];
+    float   go_down[MAX_DAYS];
     
     /* each record have a slot */
     struct {
@@ -29,6 +35,7 @@ void *alhena_module_peaklow_init( variable_t *p_config, alhena_data_t *p_data,
                                   int i_total, float *p_output_data )
 {
     peak_low_t *p_stat = NULL;
+    int i;
 
     p_stat = (peak_low_t *)calloc( 1, sizeof( peak_low_t ) );
     if( !p_stat )
@@ -36,6 +43,9 @@ void *alhena_module_peaklow_init( variable_t *p_config, alhena_data_t *p_data,
         msg_Err( "alloc peak_low_t failed" );
         return NULL;
     }
+
+    for( i=0; i<i_total; i++ )    
+        p_stat->fi[i] = fi_v( p_data, i );
 
     p_stat->i_max_stat_days = var_get_integer( p_config, "peak-low-max-days" );
 
@@ -48,57 +58,79 @@ bool alhena_module_peaklow_record_pre( void *h, alhena_data_t *p_data,
     peak_low_t *p_stat = (peak_low_t *)h;
     float f_highest, f_lowest;
     float f_flag_close = p_data->f_close[i_day];
-    int i;
+    int i_record = p_stat->i_records;
+    int i, i_start = 0;
 
     i_end = MIN( i_end, i_day + p_stat->i_max_stat_days );
     if( i_day >= i_end - 2 )
         return false;
 
-    /* init */
+#define RATIO1   (4.0f)
 
-    f_highest = 0.0;
-    f_lowest  = 8000.0; // this should be high enough
-
-    for( i=i_day+1; i<i_end; i++ )
+    for( i=1; i < i_day; i++ )
     {
-        if( p_data->f_low[i] < f_lowest )
+        // FIXME: 80
+        if( p_stat->fi[i_day-i] > avg_v( p_stat->fi, i_day - i, 80 )
+                                  + RATIO1 * dev_v( p_stat->fi, i_day - i, 80 ) )
         {
-            f_lowest = p_data->f_low[i];
-            p_stat->lowest_day2[p_stat->i_records] = i - i_day;
-        }
-
-        if( p_data->f_high[i] > f_highest )
-        {
-            f_highest = p_data->f_high[i];
-            p_stat->highest_day[p_stat->i_records] = i - i_day;
+            i_start = i_day - i;
+            break;
         }
     }
 
-    /* in percentage */
-    p_stat->lowest2[p_stat->i_records] = (f_lowest - f_flag_close) / f_flag_close;
-    p_stat->highest[p_stat->i_records] = (f_highest - f_flag_close) / f_flag_close;
+    assert( i_start );
 
-    f_lowest = 8000.0;
-    i_end = i_day + p_stat->highest_day[p_stat->i_records];
+    for( i=1; i < i_start; i++ )
+    {
+        if( p_data->f_close[i_start - i] > p_data->f_close[i_start - i + 1] )
+            break;
+
+        p_stat->up_days[i_record]++;
+    }
+
+    if( i_start + 1 == i_day )
+        p_stat->go_down[i_record] = (p_data->f_high[i_day] + p_data->f_low[i_day]) / 2;
+    else
+        p_stat->go_down[i_record] = avg_v( p_data->f_close, i_day, i_day - i_start - 1 );
+
+    p_stat->go_down[i_record] = (p_stat->go_down[i_record] - p_data->f_close[i_start]) 
+                              / p_data->f_close[i_start];
+
+    /* find lowest */
+    f_highest = .0f;
+    f_lowest  = 8000.0f;
+
+    for( i=i_day+1; i<i_end; i++ )    
+    {
+        if( p_data->f_high[i] > f_highest )
+        {
+            f_highest = p_data->f_high[i];
+            p_stat->highest_day[i_record] = i - i_day;
+        }
+    }
+
+    p_stat->highest[i_record] = (f_highest - f_flag_close) / f_flag_close;
+    i_end = i_day + p_stat->highest_day[i_record];
 
     for( i=i_day+1; i<=i_end; i++ )
     {
         if( p_data->f_low[i] < f_lowest )
         {
             f_lowest = p_data->f_low[i];
-            p_stat->lowest_day1[p_stat->i_records] = i - i_day;
+            p_stat->lowest_day1[i_record] = i - i_day;
         }
     }
 
-    p_stat->lowest1[p_stat->i_records] = (f_lowest - f_flag_close) / f_flag_close;
+    p_stat->lowest1[i_record] = (f_lowest - f_flag_close) / f_flag_close;
 
-    p_stat->day[p_stat->i_records].i_day   = p_data->day[i_day].i_day;
-    p_stat->day[p_stat->i_records].i_month = p_data->day[i_day].i_month;
-    p_stat->day[p_stat->i_records].i_year  = p_data->day[i_day].i_year;
+    p_stat->day[i_record].i_day   = p_data->day[i_day].i_day;
+    p_stat->day[i_record].i_month = p_data->day[i_day].i_month;
+    p_stat->day[i_record].i_year  = p_data->day[i_day].i_year;    
 
     p_stat->i_records++;
 
     return true;
+#undef RATIO1
 }
 
 void alhena_module_peaklow_deinit( void *h )
@@ -113,13 +145,12 @@ void alhena_module_peaklow_deinit( void *h )
                          p_stat->day[i].i_month,
                          p_stat->day[i].i_day,
                          p_stat->day[i].i_year );
-        fprintf( stdout, "%f,%d,%f,%d,%f,%d\n", 
+        fprintf( stdout, "%d,%f,%f,%d,%f\n",
+                         p_stat->up_days[i],
+                         p_stat->go_down[i],
                          p_stat->lowest1[i],
                          p_stat->lowest_day1[i],
-                         p_stat->highest[i], 
-                         p_stat->highest_day[i],
-                         p_stat->lowest2[i],
-                         p_stat->lowest_day2[i] );
+                         p_stat->highest[i] );
     }
 
     free( p_stat );
