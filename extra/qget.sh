2b5d8c900efd157fe48858ffddf460fd50542314
zhlx <zhanglongx@gmail.com>
2016-06-08 14:45:34 +0800

    qget.sh: improve last day's print

--- extra/qget.sh
+++ extra/qget.sh
@@ -2,7 +2,7 @@
 
 if test x"$1" = x"-h" -o x"$1" = x"--help" ; then
 cat <<EOF
-Usage: ./now.vol.sh
+Usage: ./qget.sh
 
 Help:
   -h, --help               print this message
@@ -16,7 +16,6 @@ exit 1
 fi
 
 alhena_dir=~/Alhena
-date=$(date -dlast-monday +%Y-%m-%d)
 
 OPTIND=1
     while getopts ":d:t:" option; do
@@ -36,15 +35,15 @@ if ! [ -d $alhena_dir/database ]; then
     exit 1
 fi
 
-if ! echo $date | egrep '[0-9]+-[0-9]+' 2>&1 > /dev/null; then
-    echo "date format error"
-    exit 1
-fi
-
 if ! [ -e $alhena_dir/database/$name.csv ]; then
     echo "$name error"
     exit 1
 fi
 
-egrep $date $alhena_dir/database/$name.csv | \
-    awk -F ',' '{printf "%s, %.2f, %d\n", $1, $2, $2 * $7 / 100000000}'
+if ! echo $date | egrep '[0-9]+-[0-9]+' 2>&1 > /dev/null; then
+    cat $alhena_dir/database/$name.csv | \
+        awk -F ',' 'END{printf "%s, %.2f, %d\n", $1, $2, $2 * $7 / 100000000}'
+else
+    egrep $date $alhena_dir/database/$name.csv | \
+        awk -F ',' '{printf "%s, %.2f, %d\n", $1, $2, $2 * $7 / 100000000}'
+fi
