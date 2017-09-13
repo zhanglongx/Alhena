691b3090e3a92dbd2eeaaa359e39b710321db075
zhlx <zhanglongx@gmail.com>
2017-09-13 13:35:02 +0800

    change: copyright

--- modules/dummy.h
+++ modules/dummy.h
@@ -1,5 +1,5 @@
 /*****************************************************************************
- * Copyright (C) 2015,2016 Alhena project
+ * Copyright (C) 2015-2017 Alhena project
  *
  * Authors: longxiao zhang <zhanglongx@gmail.com>
  *
