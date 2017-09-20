35dc691e9578463e32ae7b669940d75ac42edf74
zhlx <zhanglongx@gmail.com>
2017-09-20 18:20:24 +0800

    earning_update.sh: refine more smart

--- utilities/earning_update.sh
+++ utilities/earning_update.sh
@@ -1,9 +1,20 @@
 #! /bin/sh
 
-ALHENA_DIR=~/Programming/Alhena
+ALHENA_DIR=~/Alhena
+ALHENA_DATABASE_DIR=$ALHENA_DIR/database
 ALHENA_EXTRA_DIR=$ALHENA_DIR/extra
 
 UPDATE_FILE=earning_update.lst
+
+#
+# Command line handling
+#
+usage()
+{
+	echo "Usage: $0 START"
+
+    exit 0
+}
 
 failed_exit()
 {
@@ -11,8 +22,19 @@ failed_exit()
     exit 1
 }
 
-test -e $UPDATE_FILE || failed_exit "$UPDATE_FILE not exist"
+START=$1
 
-sed ':a;N;$!ba;s/\n/ /g' $UPDATE_FILE \
-    | xargs perl -I $ALHENA_EXTRA_DIR $ALHENA_EXTRA_DIR/earning_querier.pl \
-            -d --no-human -r -s 4 -f PE -p $ALHENA_DIR/database > /dev/null
+test x$START != x || usage
+
+test -e $ALHENA_DATABASE_DIR/$START.csv || failed_exit "$START is not in database"
+
+STOCKS=`find $ALHENA_DIR/database -name '*.csv' -exec basename {} '.csv' \; | sort -n`
+
+for s in $STOCKS; do
+    if test $s -ge $START; then
+        LIST="$LIST $s"
+    fi
+done
+
+echo $LIST | xargs perl -I $ALHENA_EXTRA_DIR $ALHENA_EXTRA_DIR/earning_querier.pl \
+                -d --no-human -r -s 4 -f PE -p $ALHENA_DIR/database > /dev/null
