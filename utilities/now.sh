d2243942536c9eb2c6350521e7e23dcd211e2737
Longxiao Zhang <zhanglongx@gmail.com>
2017-12-03 14:22:59 +0800

    utilities/now.sh: support input of .list

--- utilities/now.sh
+++ utilities/now.sh
@@ -1,12 +1,14 @@
 #! /bin/bash
 
+RESULT_FILE=result.csv
+RESULT_TMP_FILE=result.tmp.csv
+
 if test x"$1" = x"-h" -o x"$1" = x"--help" ; then
 cat <<EOF
-Usage: ./now.vol.sh
+Usage: $0 [options]
 
 Help:
   -h, --help               print this message
-
   --dir=DIR                path to Alhena
                            [~/Alhena]
   --list=FILE              stock list
@@ -16,7 +18,6 @@ fi
 
 alhena_dir=~/Alhena
 out_dir=./result
-days=3
 
 for opt do
     optarg="${opt#*=}"
@@ -26,9 +27,6 @@ for opt do
             ;;
         --list=*)
             list_file="$optarg"
-            ;;
-        --days=*)
-            days="$optarg"
             ;;
         *)
             echo "Unknown option $opt, ignored"
@@ -42,8 +40,8 @@ if [ ! -d $alhena_dir ]; then
 fi   
 
 # clean last result
-rm -f result.csv
-rm -f result.tmp.csv
+rm -f $RESULT_FILE
+rm -f $RESULT_TMP_FILE
 
 if [ -d $out_dir ]; then
     find $out_dir -name "*.csv" -exec rm -f {} \;
@@ -51,14 +49,13 @@ else
     mkdir -p $out_dir
 fi
 
-all_stock="300079"
-
 if [ x$list_file != x ] && [ -f $list_file ]; then
-    all_stock=`cat $list_file`
+    all_stock=`cat $list_file | egrep -v '^.*#'`
+    ops=`cat $list_file | sed -n 's/^.*#//p'`
 fi
 
-if [ ! `echo $days | egrep "[0-9]+"` ]; then
-    echo "days error: $days"
+if [[ x$all_stock == x ]] || [[ x$ops == x ]]; then
+    echo "list file error"
     exit 1
 fi
 
@@ -71,14 +68,16 @@ do
         continue
     fi
     
-    $alhena_dir/bin/alhena -o vol --vol-compare-days 30 -s now --now-lookback $days $file > $out_dir/$stock.csv
+    $alhena_dir/bin/alhena $ops $file > $out_dir/$stock.csv
 done 
 
 for out_file in `find $out_dir -name "*.csv"` 
 do
     name=`basename $out_file`
     name=${name/.csv/}
-    cat $out_file | sed -n "s/^now,/$name,/gp" >> result.tmp.csv
+    cat $out_file | sed -n "s/^now,/$name,/gp" >> $RESULT_TMP_FILE
 done
 
-cat result.tmp.csv | sort -nrt '-' -k 3 >> result.csv
+if [ -e $RESULT_TMP_FILE ]; then
+    cat $RESULT_TMP_FILE | sort -nrt '-' -k 3 >> $RESULT_FILE
+fi
